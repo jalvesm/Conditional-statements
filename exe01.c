038a8c374243194b0661f90b5746f45e36da1733
Joana Morais <92741380+jalvesm@users.noreply.github.com>
2022-06-17 15:15:54 -0300

    correcting bugs

--- exe01.c
+++ exe01.c
@@ -4,20 +4,6 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int main()
-{
-    int primeiro_numero;
-    setlocale(LC_ALL, "portuguese");
-    printf("Informe o número inteiro: \n");
-    scanf("%i", &primeiro_numero);
-    if (primeiro_numero % 2 == 0)
-    {
-        printf("O número informa// 1. Fazer um algoritmo que leia um numero inteiro e mostre uma mensagem indicando se este numero e par ou impar.
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-
 void runInEnglish();
 void runInPortuguese();
 void runInGerman();
@@ -106,9 +92,3 @@ void runInGerman()
     }
 }do é par. \n\n");
     }
-    else
-    {
-        printf("O número informado é impar.\n\n");
-    }
-    return 0;
-}
